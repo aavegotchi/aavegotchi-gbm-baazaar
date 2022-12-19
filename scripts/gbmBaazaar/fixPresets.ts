6f9a443022f0146465c2982c240a615d4f576676
Coderdan <dmathieuva@gmail.com>
2022-12-19 18:11:55 +0800

    Update fixPresets.ts

--- scripts/gbmBaazaar/fixPresets.ts
+++ scripts/gbmBaazaar/fixPresets.ts
@@ -1,11 +1,13 @@
-import { LedgerSigner } from "@ethersproject/hardware-wallets";
+import { LedgerSigner } from "@anders-t/ethers-ledger";
 import { ethers } from "hardhat";
+import { gasPrice } from "../helperFunctions";
 import { presets } from "./deployDiamond";
 export const GBMDiamond = "0xD5543237C656f25EEA69f1E247b8Fa59ba353306";
 async function fixPreset() {
   const signer = new LedgerSigner(ethers.provider);
   const GBM = await ethers.getContractAt("GBMFacet", GBMDiamond, signer);
-  const tx = await GBM.setAuctionPresets(0, presets[0]);
+  console.log("Setting preset");
+  const tx = await GBM.setAuctionPresets(0, presets[0], { gasPrice: gasPrice });
   console.log("Preset corrected successfully at hash", tx.hash);
 }
 
