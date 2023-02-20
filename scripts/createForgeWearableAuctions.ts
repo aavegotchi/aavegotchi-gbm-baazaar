d32e4d8657bd78c075490bc42184dff0b72cb200
orionstardust <justinono1990@outlook.com>
2023-02-20 10:52:11 -0800

    Update script for forge GBM

--- scripts/createForgeWearableAuctions.ts
+++ scripts/createForgeWearableAuctions.ts
@@ -1,7 +1,37 @@
 import { run } from "hardhat";
 import { BatchERC1155AuctionsTaskArgs } from "../tasks/createBatchERC1155Auctions";
+import {
+  CORE_BODY_COMMON,
+  CORE_BODY_GODLIKE,
+  CORE_BODY_LEGENDARY,
+  CORE_BODY_MYTHICAL,
+  CORE_BODY_RARE,
+  CORE_BODY_UNCOMMON,
+  CORE_EYES_COMMON,
+  CORE_EYES_GODLIKE,
+  CORE_EYES_LEGENDARY,
+  CORE_EYES_MYTHICAL,
+  CORE_EYES_RARE,
+  CORE_EYES_UNCOMMON,
+  CORE_FACE_GODLIKE,
+  CORE_FACE_UNCOMMON,
+  CORE_HANDS_COMMON,
+  CORE_HANDS_GODLIKE,
+  CORE_HANDS_LEGENDARY,
+  CORE_HANDS_MYTHICAL,
+  CORE_HANDS_RARE,
+  CORE_HANDS_UNCOMMON,
+  CORE_HEAD_MYTHICAL,
+  CORE_PET_COMMON,
+  CORE_PET_GODLIKE,
+  CORE_PET_LEGENDARY,
+  CORE_PET_MYTHICAL,
+  CORE_PET_RARE,
+  CORE_PET_UNCOMMON,
+} from "../helpers/constants";
 
 async function createBatchWearableAuctions() {
+  // schematic auction
   const common = [350, 351, 352, 353]; //
   const uncommon = [354, 356]; //
   const rare = [355, 357]; //
@@ -9,7 +39,7 @@ async function createBatchWearableAuctions() {
   const mythical = [362, 363, 364, 365]; //
   const godlike = [366, 367, 368, 369]; //
   const ids = [common, uncommon, rare, legendary, mythical, godlike];
-  const amounts = [1000, 500, 250, 100, 50, 5];
+  const amounts = [600, 300, 150, 0, 30, 3];
 
   const tokenIds = [];
   const tokenAmounts = [];
@@ -17,6 +47,9 @@ async function createBatchWearableAuctions() {
   const endTimes = [];
 
   for (let i = 0; i < ids.length; i++) {
+    if (amounts[i] === 0) {
+      continue;
+    }
     for (let j = 0; j < ids[i].length; j++) {
       tokenIds.push(ids[i][j]);
       tokenAmounts.push(amounts[i]);
@@ -25,9 +58,46 @@ async function createBatchWearableAuctions() {
     }
   }
 
+  // core auction
+  const coreIds = [
+    CORE_BODY_COMMON,
+    CORE_EYES_COMMON,
+    CORE_HANDS_COMMON,
+    CORE_PET_COMMON,
+    CORE_BODY_UNCOMMON,
+    CORE_EYES_RARE,
+    CORE_FACE_UNCOMMON,
+    CORE_HANDS_RARE,
+    CORE_BODY_LEGENDARY,
+    CORE_EYES_LEGENDARY,
+    CORE_HANDS_LEGENDARY,
+    CORE_PET_LEGENDARY,
+    CORE_BODY_MYTHICAL,
+    CORE_HEAD_MYTHICAL,
+    CORE_HANDS_MYTHICAL,
+    CORE_HANDS_MYTHICAL,
+    CORE_BODY_GODLIKE,
+    CORE_EYES_GODLIKE,
+    CORE_FACE_GODLIKE,
+    CORE_HANDS_GODLIKE,
+  ];
+  const coreAmounts = [
+    600, 0, 0, 300, 0, 0, 250, 0, 0, 50, 0, 0, 0, 0, 0, 0, 1, 1, 2, 2,
+  ];
+
+  for (let i = 0; i < coreIds.length; i++) {
+    if (coreAmounts[i] === 0) {
+      continue;
+    }
+    tokenIds.push(coreIds[i]);
+    tokenAmounts.push(coreAmounts[i]);
+    startTimes.push(Math.floor(Date.now() / 1000 + 200));
+    endTimes.push(Math.floor(Date.now() / 1000) + 8640);
+  }
+
   const args: BatchERC1155AuctionsTaskArgs = {
     gbmDiamondAddress: "0xD5543237C656f25EEA69f1E247b8Fa59ba353306",
-    tokenContractAddress: "0x58de9AaBCaeEC0f69883C94318810ad79Cc6a44f",
+    tokenContractAddress: "0x4fDfc1B53Fd1D80d969C984ba7a8CE4c7bAaD442", // forge diamond
     deployer: "0xFFfFfFffFFfffFFfFFfFFFFFffFFFffffFfFFFfF",
     preset: "1",
     tokenIds: tokenIds.join(),
